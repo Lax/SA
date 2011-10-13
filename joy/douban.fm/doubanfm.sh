9ef18996142942d5a0d198d4147492fc8cd994b0
Lantao Liu <liulantao@gmail.com>
2011-10-13 15:41:51 +0800

    scripts for playing music from douban.fm

---
+++ joy/douban.fm/doubanfm.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# channel list
+#
+# 华语 1
+# 欧美 2
+# 粤语 6
+# 法语 22
+# 日语 17
+# 韩语 18
+#
+# 民谣 8
+# 摇滚 7
+# 爵士 13
+# 古典 27
+# 轻音乐 9
+# 电子 14
+# R&B 16
+# 说唱 15
+# 电影原声 10
+
+channel=2
+player=`which mplayer2`
+
+if [ $# -eq 1 ]
+then
+	channel=$1
+fi
+
+
+doubanURL="http://douban.fm/j/mine/playlist?type=n&channel=${channel}"
+
+curl $doubanURL | \
+ruby -e '
+	while line = gets and line.match(/(http[^"]*\.mp3)/)
+		puts $1
+	end' | \
+xargs $player
---
+++ joy/douban.fm/play.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ $# -eq 1 ]
+then
+        channel=$1
+else
+        channel=2
+fi
+
+while true
+do
+	./doubanfm.sh $channel 2> /dev/null
+done
